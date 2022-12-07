18eea46a8a7e02b28464e1157ee073ac0670235a
Joe Rylander <rylander.j92@gmail.com>
2022-12-07 14:17:07 +0100

    longest common prefix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mergeTwoLists from "./linkedLists/mergeTwoListsByOrder.js";
 import ListNode from "./linkedLists/listNode.js";
 import reverseList from "./linkedLists/reverseList.js";
+import longestCommonPrefix from "./longestCommonPrefix.js";
 
 const list1 = new ListNode(
   6,
@@ -20,3 +21,8 @@ const list3 = new ListNode(
 // console.log(mergeTwoLists(list1, list2));
 
 // console.log(reverseList(list3));
+const strArr1 = ["dog", "racecar", "car"];
+const strArr2 = ["flower", "flow", "flight"];
+const strArr3 = ["flower", "flower", "flower"];
+// const strArr2 = ["flower", "flow"];
+console.log(longestCommonPrefix(strArr3));
---
+++ longestCommonPrefix.js
@@ -0,0 +1,8 @@
+const longestCommonPrefix = function (strs) {
+  if (!strs[0] || strs.length === 1) return strs[0] || "";
+  let i = 0;
+  while (strs[0][i] && strs.every((w) => w[i] === strs[0][i])) i++;
+  return strs[0].substring(0, i);
+};
+
+export default longestCommonPrefix;
